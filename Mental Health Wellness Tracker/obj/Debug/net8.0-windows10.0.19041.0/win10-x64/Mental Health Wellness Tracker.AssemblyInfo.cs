37483ad4abb56599220bdb0af6ca707d588aeff0
Ling Xun Sheng <jianjinbao112@gmail.com>
2025-12-09 14:43:38 +0800

    contact page

--- Mental Health Wellness Tracker/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Mental Health Wellness Tracker.AssemblyInfo.cs
+++ Mental Health Wellness Tracker/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Mental Health Wellness Tracker.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9e674602fc3bc49b752111b8d2d39f28c6908f31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+26d3553fed194425329c5f69d2968076a9968140")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

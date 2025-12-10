4142a3e89fc4aac2e302bad3fb423e91d55a559e
patseal <brandylaujunhan@gmail.com>
2025-12-10 19:36:27 +0800

    add the firebase installation

--- Mental Health Wellness Tracker/obj/Debug/net8.0-ios/Mental Health Wellness Tracker.AssemblyInfo.cs
+++ Mental Health Wellness Tracker/obj/Debug/net8.0-ios/Mental Health Wellness Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d3553fed194425329c5f69d2968076a9968140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37483ad4abb56599220bdb0af6ca707d588aeff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mental Health Wellness Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
